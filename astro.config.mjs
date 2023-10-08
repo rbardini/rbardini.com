450107a36bb6ea742a72db5cf7ac7f7b1f7362ca
Rafael Bardini <rafael@rbardini.com>
2023-10-08 02:56:56 +0200

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config'
 import pkg from './package.json' assert { type: 'json' }
 
 export default defineConfig({
-  compressHTML: true,
   integrations: [sitemap()],
   markdown: {
     shikiConfig: { theme: 'vitesse-dark' },
