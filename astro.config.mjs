2e50a14ff261ba93439ccee33a12510ce0be2f24
Rafael Bardini <rafael@rbardini.com>
2023-11-13 00:39:11 +0100

    Add light and dark code themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import pkg from './package.json' assert { type: 'json' }
 export default defineConfig({
   integrations: [sitemap()],
   markdown: {
-    shikiConfig: { theme: 'vitesse-dark' },
+    shikiConfig: {
+      theme: 'vitesse-dark',
+      experimentalThemes: {
+        light: 'vitesse-light',
+        dark: 'vitesse-dark',
+      },
+    },
   },
   site: pkg.homepage,
 })
